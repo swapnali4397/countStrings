9850b717052b2f6a79352fbc7aa71aa0a58bd71a
Swapnali Dukhande <54582170+swapnali4397@users.noreply.github.com>
2022-04-13 17:52:51 +0530

    Update countStrings.py

--- countStrings.py
+++ countStrings.py
@@ -1,14 +1,13 @@
-def countStrings(self, n):
-     MOD = 10**9 + 7
-	    a=[0 for i in range(n)]
-	    b=[0 for i in range(n)]
-	    a[0] = b[0] = 1
-	    for i in range(1,n):
-	        a[i] = (a[i-1]%MOD + b[i-1]%MOD )%MOD
-	        b[i] = a[i-1] %MOD
-     
-     return (a[n-1]%MOD + b[n-1]%MOD )%MOD;
- 
- 
-print(countStrings(3))
+def countStrings(n):
+    
+    MOD = 10**9 + 7
+    a=[0 for i in range(n)]
+    b=[0 for i in range(n)]
+    a[0] = b[0] = 1
 
+    for i in range(1,n):
+        a[i] = (a[i-1]%MOD + b[i-1]%MOD )%MOD
+        b[i] = a[i-1] %MOD
+    return (a[n-1]%MOD + b[n-1]%MOD )%MOD
+
+print(countStrings(3))
